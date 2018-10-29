e28a499844c57f18829c26a91537999231eff6a4
Mandy <mandyjjoles@gmail.com>
2018-10-28 22:57:04 -0500

    updated seeded data to include devoured

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,6 @@
-INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger',true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger',false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger',true);
+
+SELECT * FROM burgers;
 
