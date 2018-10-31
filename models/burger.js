d2e240cf26e4b38004f28745991b55159fcd263e
Mandy <mandyjjoles@gmail.com>
2018-10-31 12:30:14 -0500

    close to finishing

--- models/burger.js
+++ models/burger.js
@@ -4,22 +4,19 @@ var orm = require("../config/orm.js");
 var burger = {
   all: function(cb) {
     console.log("burger model gets all")
-    orm.all(function(res) {
+    orm.all("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
+  insertIntoValues: function(name,cb){
+    orm.insertIntoValues('burgers',name,cb);
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
+  updateSetWhere: function(id,cb){
+    orm.updateSetWhere('burgers',id,cb);
+  },
 };
+
 
 // Export the database functions for the controller (catsController.js).
 module.exports = burger;
